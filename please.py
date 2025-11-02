e2300fb432f2c3efd85e9c553e685f300ced05f7
sotiris-st <sotiriospantelistamou@gmail.com>
2025-11-02 05:47:57 +0200

    ss

---
+++ please.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="hello_world",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+    start >> end
